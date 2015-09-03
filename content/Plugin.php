52c67e6284a2b9cb9fe700c4ee32a71574d47908
Boudewijn Schoon <boudewijn@zicht.nl>
2015-09-03 11:03:22 +0000

    initial commit
    git-svn-id: svn://zon/projecten/zicht-libraries/z-plugin-messages/trunk@68310 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ content/Plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * For licensing information, please see the LICENSE file accompanied with this file.
+ *
+ * @author Gerard van Helden <drm@melp.nl>
+ * @copyright 2012 Gerard van Helden <http://melp.nl>
+ */
+
+namespace Zicht\Tool\Plugin\Content;
+
+use \Zicht\Tool\Plugin as BasePlugin;
+use \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+/**
+ * Messages plugin
+ */
+class Plugin extends BasePlugin
+{
+}
